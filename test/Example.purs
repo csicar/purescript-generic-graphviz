e699ed2a6040fccdb16839676d4ebe815610b35b
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-11-29 23:53:30 +0100

    move and updated library example

---
+++ test/Example.purs
@@ -0,0 +1,17 @@
+module Test.Example where
+
+import Data.DotLang (class GraphRepr, toGraph)
+import Data.Function (($))
+import Data.Generic.Rep (class Generic)
+import Data.GenericGraph (class Edges, genericEdges, genericToGraph)
+import Graphics.Graphviz (Engine(..), renderToSvg)
+
+data Tree' a = Leaf' | Node' (Tree' a) a (Tree' a)
+
+derive instance treeGeneric :: Generic (Tree' a) _
+
+instance treeEdges :: Edges a => Edges (Tree' a) where edges x = genericEdges x
+instance treeDotRepr ::  Edges a => GraphRepr (Tree' a) where toGraph = genericToGraph
+
+example = renderToSvg Dot $ toGraph $
+  Node' Leaf' 3 (Node' (Node' Leaf' 5 Leaf') 4 Leaf')
